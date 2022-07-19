1dc028d894d633d134a843d10e6a0cfeb0a6b7db
hagayasu <104057668+hagayasu@users.noreply.github.com>
2022-07-20 06:32:02 +0900

    ユーザー画面にユーザー名、アドレス表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users
 
-  devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
- end
+ # devise_scope :user do
+ # get '/users/sign_out' => 'devise/sessions#destroy'
+ #end
 end
